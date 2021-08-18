c2ece5d0b5ddfa8b31f539f02171613f2471a698
KJI-DEC <kji_dec22@ewhain.net>
2021-08-18 17:48:56 +0900

    Update 9095

---
+++ boj/9095 1,2,3 더하기.py
@@ -0,0 +1,19 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+def cnt(re):
+    if re == 1:
+        return 1
+    elif re == 2:
+        return 2
+    elif re == 3:
+        return 4
+    elif re < 0:
+        return 0
+    else:
+        return cnt(re - 1) + cnt(re - 2) + cnt(re -3)
+
+for i in range(t):
+    n = int(sys.stdin.readline())
+    print(cnt(n))
