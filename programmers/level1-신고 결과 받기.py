c9bd2cb82b5fd66dd40b1d51756545d7b89f20ed
KJI-DEC <kji_dec22@ewhain.net>
2022-10-20 14:39:59 +0900

    Update level1-신고 결과 받기.py

--- programmers/level1-신고 결과 받기.py
+++ programmers/level1-신고 결과 받기.py
@@ -28,3 +28,17 @@ def solution(id_list, report, k):
         answer.append(tmp)
 
     return answer
+
+# 다른 사람 풀이... 이렇게 했어야...
+# def solution(id_list, report, k):
+#     answer = [0] * len(id_list)
+#     reports = {x : 0 for x in id_list}
+#
+#     for r in set(report):
+#         reports[r.split()[1]] += 1
+#
+#     for r in set(report):
+#         if reports[r.split()[1]] >= k:
+#             answer[id_list.index(r.split()[0])] += 1
+#
+#     return answer
